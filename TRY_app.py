7f43fb45775275380c37d7f30fcb7828261cf5ef
Rahma Tarek <87337958+RaahmaTarek@users.noreply.github.com>
2023-04-22 23:37:30 +0200

    Update TRY_app.py

--- TRY_app.py
+++ TRY_app.py
@@ -5,7 +5,6 @@ from PIL import Image
 
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
-st.subheader('Was the tutorial helpful?')
 
 excel_file = 'Survey_Results.xlsx'
 sheet_name = 'DATA'
